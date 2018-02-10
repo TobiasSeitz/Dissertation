6eeed2369947fa0d970329d0a4dbf59607305922
Tobi Seitz <tobi@tobitobi.de>
2018-02-10 16:13:29 +0100

    personality study 2 v1

--- statistics/personality/study-02/comparison.R
+++ statistics/personality/study-02/comparison.R
@@ -17,8 +17,8 @@ source("../../util.R");
 d_comp100 <- read.csv("data-comparison-N100.csv", sep = ";", dec = ".")
 
 ### factorize things
-d_comp100$D_Gender <- factor(d100$D_Gender,levels=c("Male","Female"),labels=c("Male","Female"))
-d_comp100$D_ComputerScienceBackground <- factor(d100$D_ComputerScienceBackground, levels=c("No","Yes"), labels=c("No","Yes"))
+d_comp100$D_Gender <- factor(d_comp100$D_Gender,levels=c("Male","Female"),labels=c("Male","Female"))
+d_comp100$D_ComputerScienceBackground <- factor(d_comp100$D_ComputerScienceBackground, levels=c("No","Yes"), labels=c("No","Yes"))
 
 
 responseVariablesComparison <- c("C_Length", "C_Strength",
@@ -33,13 +33,13 @@ predictorsSeBIS <- list("SeBIS_Securement","SeBIS_Awareness", "SeBIS_Updating", 
 predictorsGDMS <- list("GDMS_Rational","GDMS_Intuitive", "GDMS_Avoidant",  "GDMS_Dependent", "GDMS_Spontaneous")
 
 
-autoModelsComparisonB5 <- lapply(responseVariablesComparison, getSmoothedGAM, predictors=predictorsB5, d<-d_comp100)
-autoModelsComparisonGDMS <- lapply(responseVariablesComparison, getSmoothedGAM, predictors=predictorsGDMS, d<-d_comp100)
-autoModelsComparisonSeBIS <- lapply(responseVariablesComparison, getSmoothedGAM, predictors=predictorsSeBIS, d<-d_comp100)
+autoModelsComparisonB5 <- lapply(responseVariablesComparison, getSmoothedGAM, predictors=predictorsB5, d<-d_comp100, k=8)
+#autoModelsComparisonGDMS <- lapply(responseVariablesComparison, getSmoothedGAM, predictors=predictorsGDMS, d<-d_comp100)
+#autoModelsComparisonSeBIS <- lapply(responseVariablesComparison, getSmoothedGAM, predictors=predictorsSeBIS, d<-d_comp100)
 
 updatedModelsComparisonB5 <- lapply(autoModelsComparisonB5, simplifyGAM)
-updatedModelsComparisonGDMS <- lapply(autoModelsComparisonGDMS, simplifyGAM)
-updatedModelsComparisonSeBIS <- lapply(autoModelsComparisonSeBIS, simplifyGAM)
+#updatedModelsComparisonGDMS <- lapply(autoModelsComparisonGDMS, simplifyGAM)
+#updatedModelsComparisonSeBIS <- lapply(autoModelsComparisonSeBIS, simplifyGAM)
 
 # B5
 lapply(updatedModelsComparisonB5, generatePDF, 
@@ -56,7 +56,7 @@ lapply(updatedModelsComparisonSeBIS, generatePDF,
 
 ## not using lapply now because of multi-core processor race conditions... ?
 for(i in updatedModelsComparisonB5) {
-  outputSummary(i, prefix="comparison-b5-", path="summaries")
+  outputSummary(i, prefix="comparison-b5-", path="summaries/reml")
 }
 for(i in updatedModelsComparisonGDMS) {
   outputSummary(i, prefix="comparison-gdms-", path="summaries")
