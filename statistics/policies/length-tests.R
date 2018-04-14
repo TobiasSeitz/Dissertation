44ac3bd731b5500a1e2a15d40c444c21ecc684a8
Tobi Seitz <tobi@tobitobi.de>
2018-04-14 17:43:09 +0200

    auto commit Sat Apr 14 17:43:09 CEST 2018

--- statistics/policies/length-tests.R
+++ statistics/policies/length-tests.R
@@ -19,17 +19,19 @@ p_min <- p_min +
   geom_violin()+
   coord_flip()+
   scale_y_continuous(name="Minimum Number of Characters", breaks=c(1,2,3,4,5,6,7,8,9,10,11))+
-  theme(axis.title.y=element_blank(),axis.text.y=element_blank())
+  labs(x="Website Density") +
+  theme(axis.text.y=element_blank()) 
 
 p_max <- ggplot(distr_length_max, aes(factor(Bound),n))
 p_max <- p_max + 
   geom_violin()+
   coord_flip()+
   scale_y_continuous(name="Maximum Number of Characters", breaks=c(0,10,20,30,40,50,60,80,100,150))+
-  theme(axis.title.y=element_blank(),axis.text.y=element_blank())
+  labs(x="Website Density") +
+  theme(axis.text.y=element_blank()) 
 
-p_max <- p_max + theme(text=element_text(family="ArialMT"))
-p_min <- p_min + theme(text=element_text(family="ArialMT"))
+(p_max <- p_max + theme(text=element_text(family="ArialMT")))
+(p_min <- p_min + theme(text=element_text(family="ArialMT")))
 
 ggsave(plot=p_max, filename="length-max-violin.pdf", path="policies", width=11, height=2.0)
 embed_fonts("policies/length-max-violin.pdf", outfile="policies/length-max-violin.pdf")
