f244e690e226bf6c63629e5844b6d35877a9a07c
Tobi Seitz <tobi@tobitobi.de>
2018-04-15 16:38:04 +0200

    auto commit Sun Apr 15 16:38:04 CEST 2018

--- statistics/pasdjo/score-distribution.R
+++ statistics/pasdjo/score-distribution.R
@@ -1,17 +1,33 @@
 ## score distributions
+library(ggplot2)
 
-v2LongForm <- read.table("../data/pasdjo-rounds-v2.csv", header=TRUE, 
+source("../util.R")
+
+v1LongForm <- read.table("data/dataset-v1/rounds.csv", header=TRUE, 
+                         sep=",", na.strings="NA", dec=".", strip.white=TRUE)
+
+v2LongForm <- read.table("data/pasdjo-rounds-v2.csv", header=TRUE, 
               sep=",", na.strings="NA", dec=".", strip.white=TRUE)
 
 
-p <- ggplot(v2LongForm, aes(condition));
-p <- p + geom_bar(aes(fill=factor(score)), position = position_stack(reverse = TRUE)) + 
-  coord_flip() + 
-  theme(legend.position = "top", text = element_text(size=20)) + 
-  labs(x="Condition",y="N") +
-  scale_fill_discrete(name="Score")
+(v1ScoreDistributionPlot <- ggplot(v1LongForm, aes(condition)) +
+    geom_bar(aes(fill=factor(score)), position = position_stack(reverse = TRUE), color="white") + 
+    coord_flip() + 
+    theme(legend.position = "top", text = element_text(size=20)) + 
+    labs(x="Condition",y="N") +
+    scale_fill_discrete(name="Score") +
+    theme(text=element_text(family="ArialMT"))
+  );
 
-p <- p + theme(text=element_text(family="ArialMT"))
-ggsave(plot=p, filename="score-distribution-v2.pdf", path="../graphs", width=8.4, height=2.4)
-embed_fonts("../graphs/score-distribution-v2.pdf", outfile="../graphs/score-distribution-v2.pdf")
-p
+
+savePlot(v1ScoreDistributionPlot, "score-distribution-v1.pdf", path="graphs", width=8.4, height=3.6)
+
+(v2ScoreDistributionPlot <- ggplot(v2LongForm, aes(condition)) +
+  geom_bar(aes(fill=factor(score)), position = position_stack(reverse = TRUE), color="white") + 
+    coord_flip() + 
+    theme(legend.position = "top", text = element_text(size=20)) + 
+    labs(x="Condition",y="N") +
+    scale_fill_discrete(name="Score") + 
+    theme(text=element_text(family="ArialMT"))
+)
+savePlot(v2ScoreDistributionPlot, "score-distribution-v2.pdf", path="graphs", width=8.4, height=2.4)
